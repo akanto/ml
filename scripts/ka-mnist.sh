fdc3b75e56936df650f468a2a6c58b715a0ee388
Attila Kanto <attila.kanto@gmail.com>
2017-02-06 20:00:46 +0100

    Build mnist example

--- scripts/ka-mnist.sh
+++ scripts/ka-mnist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-get_mnist_example() {
-  su -c "cd /home/spark && git clone https://github.com/akanto/ml.git && cd ml && git checkout hcc-cuda-blog" spark
+build_mnist_example() {
+  su -c "cd /home/spark && git clone https://github.com/akanto/ml.git && cd ml && git checkout hcc-cuda-blog && ./gradlew clean build fatjar" spark
 }
 
 main() {
-    get_mnist_example
+    build_mnist_example
 }
 
 [[ "$0" == "$BASH_SOURCE" ]] && main "$@"
